bc84edcdbe65a4742649bd0d93d0ac930888cc2f
Salman Rahaman Jony <jonyislamcmt@gmail.com>
2024-06-06 09:50:49 +0600

    [update] preRequisites course and added

--- src/app/modules/Course/course.service.ts
+++ src/app/modules/Course/course.service.ts
@@ -53,7 +53,37 @@ const updateCourseIntoDB = async (id: string, payload: Partial<TCourse>) => {
     },
   );
 
-  return updatedBasicCourseInfo;
+  // check if there is any pre requisites course to updated
+  if (preRequisiteCourses && preRequisiteCourses.length > 0) {
+    // filter out the deleted fields
+    const deletedPreRequisites = preRequisiteCourses
+      ?.filter((el) => el.course && el.isDeleted)
+      .map((el) => el.course);
+
+    const deletedPreRequisitesCourses = await CourseModel.findByIdAndUpdate(
+      id,
+      {
+        $pull: {
+          preRequisiteCourses: { course: { $in: deletedPreRequisites } },
+        },
+      },
+    );
+
+    // FILTER OUT THE NEW
+    const newPreRequisites = preRequisiteCourses?.filter(
+      (el) => el.course && !el.isDeleted,
+    );
+
+    const newPreRequisitesCourses = await CourseModel.findByIdAndUpdate(id, {
+      $addToSet: { preRequisiteCourses: { $each: newPreRequisites } },
+    });
+  }
+
+  const result = await CourseModel.findById(id).populate(
+    'preRequisiteCourses.course',
+  );
+
+  return { updatedBasicCourseInfo, result };
 };
 
 export const CourseServices = {
